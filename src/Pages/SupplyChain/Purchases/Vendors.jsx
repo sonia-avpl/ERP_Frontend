7840e263682642ce5c3dd6a8e07c05ec571bde03
vineetsphaviation <vineetsphaviation@gmail.com>
2025-06-27 13:28:48 +0530

    vendor page

--- src/Pages/SupplyChain/Purchases/Vendors.jsx
+++ src/Pages/SupplyChain/Purchases/Vendors.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Plus } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Vendors = () => {
+  const navigate = useNavigate();
   return (
-    <div>Vendors</div>
-  )
-}
+    <>
+      <div className="flex items-center justify-between p-4 border-b">
+        <div className="text-lg font-semibold">All Items</div>
+        <button
+          onClick={() => navigate("/inventory/vendors/new")}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded flex items-center gap-1 text-sm"
+        >
+          <Plus className="w-4 h-4" />
+          New
+        </button>
+      </div>
+    </>
+  );
+};
 
-export default Vendors
+export default Vendors;
