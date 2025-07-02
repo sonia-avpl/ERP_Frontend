3dc8fab922e8d8e3a54105c009ff9c7a4c104e77
vineetsphaviation <vineetsphaviation@gmail.com>
2025-07-02 15:27:45 +0530

    Link tag

--- src/Pages/SupplyChain/Purchases/Vendors.jsx
+++ src/Pages/SupplyChain/Purchases/Vendors.jsx
@@ -2,6 +2,7 @@ import { Plus } from "lucide-react";
 import { useState } from "react";
 import VendorForm from "../../../components/form/VendorForm";
 import { useGet } from "../../../hooks/useGet";
+import { Link } from "react-router-dom";
 
 const Vendors = () => {
   const [showVendorForm, setShowVendorForm] = useState(false);
@@ -80,9 +81,11 @@ const Vendors = () => {
                     Rs.{Math.floor(Math.random() * 700)}
                   </td>
                   <td className="px-4 py-2 text-center">
-                    <button className="ml-2 text-blue-500 hover:text-blue-700 text-sm">
-                      View
-                    </button>
+                    <Link to={`/vendors/${vendor._id}`}>
+                      <button className="ml-2 text-blue-500 hover:text-blue-700 text-sm">
+                        View
+                      </button>
+                    </Link>
                   </td>
                 </tr>
               ))}
