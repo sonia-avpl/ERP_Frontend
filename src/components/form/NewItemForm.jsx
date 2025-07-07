cab73c541efafc50459fc99942506befc30cd88a
vineetsphaviation <vineetsphaviation@gmail.com>
2025-07-07 16:55:08 +0530

    newitemfom

--- src/components/form/NewItemForm.jsx
+++ src/components/form/NewItemForm.jsx
@@ -548,13 +548,6 @@ const NewItemForm = ({ onClose }) => {
           >
             Save
           </button>
-
-          {loading && (
-            <p className="text-sm text-gray-500">Loading vendors...</p>
-          )}
-          {error && (
-            <p className="text-sm text-red-500">Failed to load vendors</p>
-          )}
         </div>
       </div>
     </section>
