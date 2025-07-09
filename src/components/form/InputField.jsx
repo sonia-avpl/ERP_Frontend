3e4787a894d0b15497d832114baefe59ffef161a
vineetsphaviation <vineetsphaviation@gmail.com>
2025-07-09 15:06:22 +0530

    InputField

--- src/components/form/InputField.jsx
+++ src/components/form/InputField.jsx
@@ -1,19 +1,31 @@
-const InputField = ({ label, type, name, value, onChange, required = false,placeholder="" }) => {
+const InputField = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  required = false,
+  placeholder = "",
+  endIcon,
+}) => {
   return (
     <div>
       <label className="block text-sm font-semibold mb-1">
         {label} {required && <span className="text-red-500">*</span>}
       </label>
-      <input
-        type={type}
-        name={name}
-        value={value}
-        onChange={onChange}
-        className="w-full p-2 border border-gray-300 rounded-md"
-        required={required}
-        aria-required={required}
-        placeholder={placeholder}
-      />
+      <div className="relative">
+        <input
+          type={type}
+          name={name}
+          value={value}
+          onChange={onChange}
+          required={required}
+          aria-required={required}
+          placeholder={placeholder}
+          className="w-full px-3 py-2 pr-10 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        {endIcon && endIcon}
+      </div>
     </div>
   );
 };
